295e00d549d4efc77fcd0b86b32d7e16b1003679
YoshidaTakeshi <yoshida.takeshi.infratop@gmail.com>
2021-05-25 00:13:33 +0900

    create population chart component

---
+++ components/PopulationChart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Line, mixins } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  mixins: [mixins.reactiveProp],
+  props: {
+    chartData: {
+      type: Object,
+      default: null,
+    },
+    options: {
+      type: Object,
+      default: null,
+    },
+  },
+  watch: {
+    // chartDataが更新された時チャートを描き直す
+    chartData() {
+      this.renderChart(this.chartData, this.options)
+    },
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options)
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,14 +7,21 @@
       @check="getPopulation($event)"
       @uncheck="deletePopulation($event)"
     />
+    <div class="small">
+      <PopulationChart :chart-data="datacollection" :options="options" />
+    </div>
   </div>
 </template>
 
 <script>
+import * as palette from 'google-palette'
 import PrefectureCheckbox from '~/components/PrefectureCheckbox.vue'
+import PopulationChart from '~/components/PopulationChart.vue'
+
 export default {
   components: {
     PrefectureCheckbox,
+    PopulationChart,
   },
   async asyncData(context) {
     const result = await context.$axios.get('api/v1/prefectures')
@@ -24,6 +31,32 @@ export default {
   data() {
     return {
       checkedPrefs: [],
+      datacollection: {
+        labels: [],
+        datasets: [],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: '年度',
+              },
+            },
+          ],
+          yAxes: [
+            {
+              scaleLabel: {
+                display: true,
+                labelString: '人口数',
+              },
+            },
+          ],
+        },
+      },
     }
   },
   methods: {
@@ -44,7 +77,7 @@ export default {
         populationInfo: result.data.result,
       }
       this.checkedPrefs.push(prefPopulation)
-      console.log(this.checkedPrefs)
+      this.updateDatacollection(this.checkedPrefs)
     },
     deletePopulation(targetPref) {
       // checkedPrefsからチェックを外した県を削除
@@ -53,7 +86,31 @@ export default {
           this.checkedPrefs.splice(index, 1)
         }
       })
-      console.log(this.checkedPrefs)
+      this.updateDatacollection(this.checkedPrefs)
+    },
+    updateDatacollection(checkedPrefs) {
+      const newDatacollection = {}
+      newDatacollection.datasets = []
+      // グラフに割り当てる色を自動生成
+      const borderColors = palette('mpn65', this.checkedPrefs.length).map(
+        (hex) => {
+          return '#' + hex
+        }
+      )
+      checkedPrefs.forEach((checkedPref, index) => {
+        if (index === 0) {
+          newDatacollection.labels =
+            checkedPref.populationInfo.data[0].data.map((d) => d.year)
+        }
+        const chartData = {
+          label: checkedPref.prefName,
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          borderColor: borderColors[index],
+          data: checkedPref.populationInfo.data[0].data.map((d) => d.value),
+        }
+        newDatacollection.datasets.push(chartData)
+      })
+      this.datacollection = newDatacollection
     },
   },
 }
@@ -80,5 +137,10 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.small {
+  max-width: 600px;
+  margin: 150px auto;
 }
 </style>
